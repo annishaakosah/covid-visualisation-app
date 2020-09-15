68a1b21f799d953e117e9c1930d7e36ab07d4322
James Del Puerto <delpujame@ecs.vuw.ac.nz>
2020-09-15 23:36:45 +1200

    Fixed map resize bug

--- src/views/ChoroplethMap.js
+++ src/views/ChoroplethMap.js
@@ -5,149 +5,161 @@ import L from 'leaflet';
 // import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import geonet from './../data/custom.geoM';
 
-  function ChoroplethMap() {
+function ChoroplethMap() {
     useEffect(() => {
-      var map = L.map('map', {
-        center: [51.505, -0.09],
-        minZoom: 2,
-        maxZoom: 6,
-        maxBounds: [[-90, -180], [90, 180]]
-      }).setView([51.505, -0.09], 3);
-      console.log(map.getBounds())
-      map.on('zoom', () => {
-        console.log(map.getZoom());
-      })
+        // var container = L.DomUtil.get('map');
+        // if (container != null) {
+        //     container.off();
+        //     // container._leaflet_id = null;
+        // }
+        // document.getElementById('container').innerHTML = "<div id='map' style='width: 100%; height: 100%;'></div>";
+        var map = L.map('map', {
+            center: [51.505, -0.09],
+            minZoom: 2,
+            maxZoom: 6,
+            maxBounds: [[-90, -180], [90, 180]]
+        }).setView([51.505, -0.09], 3);
+        console.log(map.getBounds())
+        map.on('zoom', () => {
+            console.log(map.getZoom());
+        })
 
-      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox/dark-v9',
-        tileSize: 512,
-        zoomOffset: -1,
-        noWrap: true,
-        bounds: [
-          [-90, -180],
-          [90, 180]
-        ],
-        bounceAtZoomLimits: true
-      }).addTo(map);
-      // control that shows state info on hover
-      var info = L.control();
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: 'mapbox/dark-v9',
+            tileSize: 512,
+            zoomOffset: -1,
+            noWrap: true,
+            bounds: [
+                [-90, -180],
+                [90, 180]
+            ],
+            bounceAtZoomLimits: true
+        }).addTo(map);
+        // control that shows state info on hover
+        var info = L.control();
 
-      info.onAdd = function (map) {
-        this._div = L.DomUtil.create('div', 'info');
-        this.update();
-        return this._div;
-      };
-
-      info.update = function (props) {
-        this._div.innerHTML = '<h4>Global COVID-19 Density</h4>' + (props ?
-          '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
-          : 'Hover over a country');
-      };
-
-      info.addTo(map);
-
-
-      // get color depending on population density value
-      const getColor = (d) => {
-        return d > 1000 ? '#800026' :
-          d > 500 ? '#BD0026' :
-            d > 200 ? '#E31A1C' :
-              d > 100 ? '#FC4E2A' :
-                d > 50 ? '#FD8D3C' :
-                  d > 20 ? '#FEB24C' :
-                    d > 10 ? '#FED976' :
-                      '#FFEDA0';
-      }
-
-      function style(feature) {
-        return {
-          weight: 1,
-          opacity: 1,
-          color: 'white',
-          dashArray: '3',
-          fillOpacity: 0.7,
-          // EDIT
-          // fillColor: getColor(feature.properties.density)
-          fillColor: 'blue'
+        info.onAdd = function (map) {
+            this._div = L.DomUtil.create('div', 'info');
+            this.update();
+            return this._div;
         };
-      }
 
-      function highlightFeature(e) {
-        var layer = e.target;
+        info.update = function (props) {
+            this._div.innerHTML = '<h4>Global COVID-19 Density</h4>' + (props ?
+                '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+                : 'Hover over a country');
+        };
 
-        layer.setStyle({
-          weight: 5,
-          color: '#666',
-          dashArray: '',
-          fillOpacity: 0.7
-        });
+        info.addTo(map);
 
-        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-          layer.bringToFront();
+
+        // get color depending on population density value
+        const getColor = (d) => {
+            return d > 1000 ? '#800026' :
+                d > 500 ? '#BD0026' :
+                    d > 200 ? '#E31A1C' :
+                        d > 100 ? '#FC4E2A' :
+                            d > 50 ? '#FD8D3C' :
+                                d > 20 ? '#FEB24C' :
+                                    d > 10 ? '#FED976' :
+                                        '#FFEDA0';
         }
 
-        info.update(layer.feature.properties);
-      }
-
-      var geojson;
-
-      function resetHighlight(e) {
-        geojson.resetStyle(e.target);
-        info.update();
-      }
-
-      function zoomToFeature(e) {
-        map.fitBounds(e.target.getBounds());
-      }
-
-      function onEachFeature(feature, layer) {
-        layer.on({
-          mouseover: highlightFeature,
-          mouseout: resetHighlight,
-          click: zoomToFeature
-        });
-      }
-
-      geojson = L.geoJson(geonet, {
-        style: style,
-        onEachFeature: onEachFeature
-      }).addTo(map);
-
-      map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
-
-      var legend = L.control({ position: 'bottomright' });
-
-      legend.onAdd = function (map) {
-
-        var div = L.DomUtil.create('div', 'info legend'),
-          grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-          labels = [],
-          from, to;
-
-        for (var i = 0; i < grades.length; i++) {
-          from = grades[i];
-          to = grades[i + 1];
-
-          labels.push(
-            '<i style="background:' + getColor(from + 1) + '"></i> ' +
-            from + (to ? '&ndash;' + to : '+'));
+        function style(feature) {
+            return {
+                weight: 1,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 0.7,
+                // EDIT
+                // fillColor: getColor(feature.properties.density)
+                fillColor: 'blue'
+            };
         }
 
-        div.innerHTML = labels.join('<br>');
-        return div;
-      };
+        function highlightFeature(e) {
+            var layer = e.target;
 
-      legend.addTo(map);
+            layer.setStyle({
+                weight: 5,
+                color: '#666',
+                dashArray: '',
+                fillOpacity: 0.7
+            });
+
+            if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+                layer.bringToFront();
+            }
+
+            info.update(layer.feature.properties);
+        }
+
+        var geojson;
+
+        function resetHighlight(e) {
+            geojson.resetStyle(e.target);
+            info.update();
+        }
+
+        function zoomToFeature(e) {
+            map.fitBounds(e.target.getBounds());
+        }
+
+        function onEachFeature(feature, layer) {
+            layer.on({
+                mouseover: highlightFeature,
+                mouseout: resetHighlight,
+                click: zoomToFeature
+            });
+        }
+
+        geojson = L.geoJson(geonet, {
+            style: style,
+            onEachFeature: onEachFeature
+        }).addTo(map);
+
+        map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
+
+        var legend = L.control({ position: 'bottomright' });
+
+        legend.onAdd = function (map) {
+
+            var div = L.DomUtil.create('div', 'info legend'),
+                grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+                labels = [],
+                from, to;
+
+            for (var i = 0; i < grades.length; i++) {
+                from = grades[i];
+                to = grades[i + 1];
+
+                labels.push(
+                    '<i style="background:' + getColor(from + 1) + '"></i> ' +
+                    from + (to ? '&ndash;' + to : '+'));
+            }
+
+            div.innerHTML = labels.join('<br>');
+            return div;
+        };
+
+        legend.addTo(map);
+        return () => {
+            if (map) {
+                map.remove("leaflet-container");
+                // map.remove(); https://michalzalecki.com/versatility-and-use-cases-of-react-use-effect-hook/
+            }
+        };
     });
 
     return (
-      <div id="map"></div>
+        <div id="map"></div>
     );
-  }
+}
 
 
 
